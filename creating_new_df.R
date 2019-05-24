9c7254a7ef97326952f47241ce3647c8f3839c2b
Alexandra <alexandra_baur94@web.de>
2019-05-24 16:45:40 +0100

    Including Feature importance

--- creating_new_df.R
+++ creating_new_df.R
@@ -177,3 +177,24 @@ fviz_pca_ind(res.pca, pointsize = "cos2",
              pointshape = 21, fill = "#E7B800",
              repel = TRUE # Avoid text overlapping (slow if many points)
 )
+
+
+# Feature importance
+
+#nk features by importance using the caret r packageR
+# ensure results are repeatable
+set.seed(7)
+# load the library
+library(mlbench)
+library(caret)
+# load the dataset
+
+control <- trainControl(method="repeatedcv", number=10, repeats=3)
+# train the model
+model <- train(Unemployment....~., data=df_pca, method="lm", preProcess="scale", trControl=control)
+# estimate variable importance
+importance <- varImp(model, scale=FALSE)
+# summarize importance
+print(importance)
+# plot importance
+plot(importance)
